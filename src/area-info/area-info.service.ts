cb02951d137481b2b5cc91e0bed6ac83bbc353e7
Maneth Pak <manethpak00@gmail.com>
2021-12-28 14:54:03 +0700

    modify service to use canitravel api

--- src/area-info/area-info.service.ts
+++ src/area-info/area-info.service.ts
@@ -1,54 +1,30 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
-import { stringify } from 'querystring';
 
 @Injectable()
 export class AreaInfoService {
   constructor(private httpService: HttpService) {}
-
-  async getApiKey(): Promise<string> {
-    const res = await this.httpService
-      .post(
-        'https://test.api.amadeus.com/v1/security/oauth2/token',
-        stringify({
-          grant_type: 'client_credentials',
-          client_id: process.env.AMADEUS_API_KEY,
-          client_secret: process.env.AMADEUS_API_SECRET,
-        }),
-        {
-          headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-          },
-        },
-      )
-      .toPromise();
-    return res.data.access_token;
-  }
-
-  async areaRegulation(countryCode: string) {
-    const apiKey = await this.getApiKey();
-    countryCode = countryCode.toUpperCase();
+  async areaRegulation(countryCode: string): Promise<object> {
     try {
       const areaInfo = await this.httpService
-        .get(
-          `https://test.api.amadeus.com/v1/duty-of-care/diseases/covid19-area-report?countryCode=${countryCode}`,
-          {
-            headers: {
-              Authorization: `Bearer ${apiKey}`,
-            },
+        .get(`https://api.canitravel.net/api/v1/countries/${countryCode}`, {
+          headers: {
+            Authorization: `Bearer ${process.env.CANITRAVEL_TOKEN}`,
           },
-        )
+        })
         .toPromise();
-      const data = areaInfo.data.data;
-      const response: object = {
-        area: data.area,
-        areaPolicy: data.areaPolicy,
-        transportation: data.areaAccessRestriction.transportation,
-        tracingApplication: data.areaAccessRestriction.tracingApplication,
-        mask: data.areaAccessRestriction.mask,
+      const data = areaInfo.data.data.travel_status;
+      const res = {
+        summary_text: data.summary_text,
+        result_text: data.result_text,
+        is_mask_required: data.is_mask_required,
+        is_mask_required_text: data.is_mask_required_text,
+        quarantine: data.quarantine,
+        covid_test_before_departure: data.covid_test_before_departure,
+        full_info_text_html: data.full_info_text_html,
       };
 
-      return response;
+      return res;
     } catch (e) {
       throw new BadRequestException();
     }
